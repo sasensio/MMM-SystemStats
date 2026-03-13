af23ecb56ba09562782baa5a71cbfd1c60e11dbb
Samuel Asensio <sam.asensio@gmail.com>
2026-03-13 09:27:04 +0100

    feat: add fan speed (PWM%) and state from fanControl service
    Read fan status from /var/log/fanControl.log written by the
    RPi GPIO PWM fan controller. Adds two new rows to the display:
    - Fan Speed: PWM duty cycle percentage (e.g. 50%)
    - Fan State: OFF / LOW / MED / HIGH label
    
    Translations updated for en, de, fr, id, sv.

--- MMM-SystemStats.js
+++ MMM-SystemStats.js
@@ -16,69 +16,71 @@ Module.register('MMM-SystemStats', {
     language: config.language,
     units: config.units,
     useSyslog: false,
-    thresholdCPUTemp: 75, // in configured units
+    thresholdCPUTemp: 75,
     baseURLSyslog: 'http://127.0.0.1:8080/syslog',
     label: 'textAndIcon'
   },
-  // Define required styles.
-  getStyles: function() {
-    return ["font-awesome.css"];
-  },
-  // Define required scripts.
-	getScripts: function () {
-      return ["moment.js", "moment-duration-format.js"];
-	},
 
-  // Define required translations.
-	getTranslations: function() {
+  getStyles: function() {
+    return ['font-awesome.css'];
+  },
+
+  getScripts: function() {
+    return ['moment.js', 'moment-duration-format.js'];
+  },
+
+  getTranslations: function() {
     return {
       'en': 'translations/en.json',
       'fr': 'translations/fr.json',
       'id': 'translations/id.json',
       'de': 'translations/de.json'
     };
-	},
+  },
 
-  // Define start sequence
   start: function() {
     Log.log('Starting module: ' + this.name);
-
-    // set locale
     moment.locale(this.config.language);
 
     this.stats = {};
-    this.stats.cpuTemp = this.translate('LOADING').toLowerCase();
-    this.stats.sysLoad = this.translate('LOADING').toLowerCase();
-    this.stats.freeMem = this.translate('LOADING').toLowerCase();
-    this.stats.upTime = this.translate('LOADING').toLowerCase();
-    this.stats.freeSpace = this.translate('LOADING').toLowerCase();
+    var loading = this.translate('LOADING').toLowerCase();
+    this.stats.cpuTemp   = loading;
+    this.stats.sysLoad   = loading;
+    this.stats.freeMem   = loading;
+    this.stats.upTime    = loading;
+    this.stats.freeSpace = loading;
+    this.stats.fanPWM    = loading;
+    this.stats.fanStatus = loading;
     this.sendSocketNotification('CONFIG', this.config);
   },
 
   socketNotificationReceived: function(notification, payload) {
-    //Log.log('MMM-SystemStats: socketNotificationReceived ' + notification);
-    //Log.log(payload);
     if (notification === 'STATS') {
       this.stats.cpuTemp = payload.cpuTemp;
-      //console.log("this.config.useSyslog-" + this.config.useSyslog + ', this.stats.cpuTemp-'+parseInt(this.stats.cpuTemp)+', this.config.thresholdCPUTemp-'+this.config.thresholdCPUTemp);
+
       if (this.config.useSyslog) {
         var cpuTemp = Math.ceil(parseFloat(this.stats.cpuTemp));
-        //console.log('before compare (' + cpuTemp + '/' + this.config.thresholdCPUTemp + ')');
         if (cpuTemp > this.config.thresholdCPUTemp) {
           console.log('alert for threshold violation (' + cpuTemp + '/' + this.config.thresholdCPUTemp + ')');
-          this.sendSocketNotification('ALERT', {config: this.config, type: 'WARNING', message: this.translate("TEMP_THRESHOLD_WARNING") + ' (' + this.stats.cpuTemp + '/' + this.config.thresholdCPUTemp + ')' });
+          this.sendSocketNotification('ALERT', {
+            config: this.config,
+            type: 'WARNING',
+            message: this.translate('TEMP_THRESHOLD_WARNING') + ' (' + this.stats.cpuTemp + '/' + this.config.thresholdCPUTemp + ')'
+          });
         }
       }
-      this.stats.sysLoad = payload.sysLoad[0];
-      this.stats.freeMem = Number(payload.freeMem).toFixed() + '%';
+
+      this.stats.sysLoad   = payload.sysLoad[0];
+      this.stats.freeMem   = Number(payload.freeMem).toFixed() + '%';
       upTime = parseInt(payload.upTime[0]);
-      this.stats.upTime = moment.duration(upTime, "seconds").humanize();
+      this.stats.upTime    = moment.duration(upTime, 'seconds').humanize();
       this.stats.freeSpace = payload.freeSpace;
+      this.stats.fanPWM    = payload.fanPWM   || 'N/A';
+      this.stats.fanStatus = payload.fanStatus || 'N/A';
       this.updateDom(this.config.animationSpeed);
     }
   },
 
-  // Override dom generator.
   getDom: function() {
     var self = this;
     var wrapper = document.createElement('table');
@@ -104,9 +106,17 @@ Module.register('MMM-SystemStats', {
         text: 'DISK_FREE',
         icon: 'fa-hdd-o',
       },
+      fanPWM: {
+        text: 'FAN_PWM',
+        icon: 'fa-cog',
+      },
+      fanStatus: {
+        text: 'FAN_STATUS',
+        icon: 'fa-adjust',
+      },
     };
 
-    Object.keys(sysData).forEach(function (item){
+    Object.keys(sysData).forEach(function(item) {
       var row = document.createElement('tr');
 
       if (self.config.label.match(/^(text|textAndIcon)$/)) {
@@ -119,7 +129,7 @@ Module.register('MMM-SystemStats', {
 
       if (self.config.label.match(/^(icon|textAndIcon)$/)) {
         var c2 = document.createElement('td');
-        c2.innerHTML = `<i class="fa ${sysData[item].icon} fa-fw"></i>`;
+        c2.innerHTML = '<i class="fa ' + sysData[item].icon + ' fa-fw"></i>';
         row.appendChild(c2);
       }
 
--- node_helper.js
+++ node_helper.js
@@ -8,36 +8,60 @@
  */
 
 const NodeHelper = require('node_helper');
-var async = require('async');
-var exec = require('child_process').exec;
-var request = require('request');
+const async = require('async');
+const exec = require('child_process').exec;
+const fs = require('fs');
+const request = require('request');
+
+const FAN_LOG = '/var/log/fanControl.log';
 
 module.exports = NodeHelper.create({
   start: function() {
     //console.log('Starting node helper: ' + this.name);
   },
 
-  // Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
     var self = this;
 
     if (notification === 'CONFIG') {
       this.config = payload;
-      // first call
       self.getStats();
-      // interval call
       setInterval(function() {
         self.getStats();
       }, this.config.updateInterval);
     }
     else if (notification === 'ALERT') {
       this.config = payload.config;
-      // notif syslog
-      //console.log('url : ' + payload.config.baseURLSyslog);
       request({ url: payload.config.baseURLSyslog + '?type=' + payload.type + '&message=' + encodeURI(payload.message), method: 'GET' }, function(error, response, body) {
-        console.log("notif MMM-syslog with response " + response.statusCode);
+        console.log('notif MMM-syslog with response ' + response.statusCode);
       });
     }
+  },
+
+  getFanStats: function() {
+    var speed = 'N/A';
+    var status = 'N/A';
+    try {
+      var log = fs.readFileSync(FAN_LOG, 'utf8');
+      // Match "fanSpeed   50" or "fanSpeed 100" (may have leading spaces)
+      var m = log.match(/fanSpeed\s+(-?\d+(?:\.\d+)?)/);
+      if (m) {
+        var pct = Math.round(parseFloat(m[1]));
+        speed = pct + '%';
+        if (pct <= 0) {
+          status = 'OFF';
+        } else if (pct < 50) {
+          status = 'LOW';
+        } else if (pct < 90) {
+          status = 'MED';
+        } else {
+          status = 'HIGH';
+        }
+      }
+    } catch (e) {
+      // log not available
+    }
+    return { fanPWM: speed, fanStatus: status };
   },
 
   getStats: function() {
@@ -45,17 +69,16 @@ module.exports = NodeHelper.create({
 
     var temp_conv = '';
     switch (this.config.units) {
-    case "imperial":
-        temp_conv = 'awk \'{printf("%.1f°F\\n",(($1*1.8)/1e3)+32)}\'';
-        break;
-    case "metric":
-        temp_conv = 'awk \'{printf("%.1f°C\\n",$1/1e3)}\'';
-        break;
-    case "default":
+    case 'imperial':
+      temp_conv = 'awk \'{printf("%.1f°F\\n",(($1*1.8)/1e3)+32)}\'';
+      break;
+    case 'metric':
+      temp_conv = 'awk \'{printf("%.1f°C\\n",$1/1e3)}\'';
+      break;
+    case 'default':
     default:
-        // kelvin
-        temp_conv = 'awk \'{printf("%.1f°K\\n",($1/1e3)+273.15)}\'';
-        break;
+      temp_conv = 'awk \'{printf("%.1f°K\\n",($1/1e3)+273.15)}\'';
+      break;
     }
 
     async.parallel([
@@ -69,20 +92,23 @@ module.exports = NodeHelper.create({
       async.apply(exec, 'cat /proc/uptime'),
       // get root free-space
       async.apply(exec, "df -h|grep /dev/root|awk '{print $4}'"),
-
     ],
-    function (err, res) {
+    function(err, res) {
       var stats = {};
       stats.cpuTemp = res[0][0];
       stats.sysLoad = res[1][0].split(' ');
       stats.freeMem = res[2][0];
       stats.upTime = res[3][0].split(' ');
-	  stats.freeSpace = res[4][0];
-      // console.log(stats);
+      stats.freeSpace = res[4][0];
+
+      // Read fan stats from fanControl log
+      var fan = self.getFanStats();
+      stats.fanPWM = fan.fanPWM;
+      stats.fanStatus = fan.fanStatus;
+
       self.sendSocketNotification('STATS', stats);
     });
   },
 
   // http://unix.stackexchange.com/questions/69185/getting-cpu-usage-same-every-time/69194#69194
-
 });
